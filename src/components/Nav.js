b0446f7af0daa888d483894ea9733896f7e22270
amroczeK <mroczek9990@gmail.com>
2021-02-25 23:21:46 +0800

    nav bar line animation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
 const Nav = () => {
+  const { pathname } = useLocation();
+  console.log(pathname);
   return (
     <StyledNav>
       <h1>
@@ -13,12 +16,27 @@ const Nav = () => {
       <ul>
         <li>
           <Link to='/'>About Us</Link>
+          <Line
+            transition={{ duration: 0.75 }}
+            initial={{ width: '0%' }}
+            animate={{ width: pathname === '/' ? '50%' : '0%' }}
+          />
         </li>
         <li>
           <Link to='/work'>Our Work</Link>
+          <Line
+            transition={{ duration: 0.75 }}
+            initial={{ width: '0%' }}
+            animate={{ width: pathname.match(/(\/work)/gim) ? '50%' : '0%' }}
+          />
         </li>
         <li>
           <Link to='/contact'>Contact Us</Link>
+          <Line
+            transition={{ duration: 0.75 }}
+            initial={{ width: '0%' }}
+            animate={{ width: pathname === '/contact' ? '50%' : '0%' }}
+          />
         </li>
       </ul>
     </StyledNav>
@@ -70,6 +88,18 @@ const StyledNav = styled.nav`
         padding: 0;
       }
     }
+  }
+`;
+
+const Line = styled(motion.div)`
+  height: 0.3rem;
+  background: #23d997;
+  width: 0%;
+  position: absolute;
+  bottom: -80%;
+  left: 60%;
+  @media (max-width: 1300px) {
+    left: 0%;
   }
 `;
 
